33b5c0e677c26df763bb14ff89bc4de0734ef20e
Creighton Magoun <magoun@gmail.com>
2017-06-27 00:02:59 +0000

    end of session commit

--- script.js
+++ script.js
@@ -1,75 +1,44 @@
 /* global $ */
 // Declare global variables.
-var board;
-var playerMarker;
-var computerMarker;
-var computerStarts;
-var computerMoves;
-var gameOver;
+var sequence = [];
 
 // Define functions.
-function chooseX() {
-	$('#game-top').html('<p>You are playing: X</p>');
-	playerMarker = 'X';
-	startGame();
-}
-
-function chooseO() {
-	$('#game-top').html('<p>You are playing: O</p>');
-	playerMarker = 'O';
-	startGame();
-}
-
-function setComputerMarker() {
-	playerMarker == 'X' ? computerMarker = 'O' : computerMarker = 'X';
-}
-
 function startGame() {
-	setComputerMarker();
-	$('#prompt').css('display','none');
-	$('#btn-land').css('display','inherit');
-	if (computerStarts) {moveComputer();}
+	// Get first move.
+	sequence.push(getNextMove());
+	
+	// Lock start button.
+	$('#start')[0].disabled = true;
+	
+	for (var i = 0; i < sequence.length; i++) {
+		console.log(sequence[i]);
+	}
+}
+
+function getNextMove() {
+	return Math.floor(Math.random() * 4);
 }
 
 function lockButtons() {
-	for (var i = 1; i < 10; i++) {
-		$('#' + i.toString())[0].disabled = true;
-	}
+	$('.overlay').css('display','inherit');
 }
 
 function unlockButtons() {
-	for (var i = 1; i < 10; i++) {
-		$('#' + i.toString())[0].disabled = false;
-	}
-}
-
-function moveComputer() {
-	var move = computerMoves.pop();
-	if (board[move] === undefined) {
-		$('#' + move.toString()).html('<p>' + computerMarker + '</p>');
-		board[move] = computerMarker;
-	} else {
-		moveComputer();
-	}
-	winCheck();
-}
-
-function progressGame() {
-	winCheck();
-	if (!gameOver) {
-		moveComputer();
-	}
+	$('.overlay').css('display','none');
 }
 
 function reset() {
-	$('#1').html('');
-	$('#2').html('');
-	$('#3').html('');
-	$('#4').html('');
+	// Unlock start button.
+	$('#start')[0].disabled = false;
+	
+	sequence = [];
 }
 
-function update1 () {
-
+function red () {
+    $('.btn-red').toggleClass("redActive");
+    var that = $('.btn-red'), delay = setTimeout(function () {
+          that.toggleClass("redActive");
+    }, 300);
 }
 
 function update2 () {
@@ -89,14 +58,15 @@ $(document).ready(function() {
 	// Initialize game state.
 
 	// Update game state for button clicked.
-	$('#x').click(chooseX);
-	$('#o').click(chooseO);
-	$('#reset').click(reset);
+	$('#start').click(startGame);
+	$('#strict').click(lockButtons);
 	
 	$("#1").click(update1);
 	$("#2").click(update2);
 	$("#3").click(update3);
 	$("#4").click(update4);
+	
+	$('#reset').click(reset);
 
 
 	// Bind buttons to key presses.
